388be89f43311bee1e7975452208c5189a0f4aba
Omar-Driouch <omardriouch29@gmail.com>
2023-10-02 15:08:19 +0100

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,3 @@
 import sys
-
-message = "and that piece of ar is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(message + '\n')
+sys.stderr.write("and that piece of ar is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
