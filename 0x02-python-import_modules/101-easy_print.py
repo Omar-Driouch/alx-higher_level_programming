582f3437508732e5cd3051e0a239ceeda072f411
Omar-Driouch <omardriouch29@gmail.com>
2023-10-05 14:07:36 +0100

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-from easy_print_101 import PythonIsCool; PythonIsCool("#pythoniscool")
+import pythoniscool
--- 0x02-python-import_modules/easy_print_101.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def PythonIsCool(str):
-    print(str)
