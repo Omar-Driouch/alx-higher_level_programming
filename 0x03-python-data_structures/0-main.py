2424fd674edde2b7e2f63db065c3063265c3d5c7
Omar-Driouch <omardriouch29@gmail.com>
2023-10-08 06:54:06 +0100

    1-element_at.py

---
+++ 0x03-python-data_structures/0-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    print_list_integer = __import__('0-print_list_integer').print_list_integer
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
+
+
