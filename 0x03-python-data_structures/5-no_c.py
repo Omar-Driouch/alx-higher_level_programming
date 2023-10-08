ccdf8f0d3de843a889026ba124af03482a2f7206
Omar-Driouch <omardriouch29@gmail.com>
2023-10-08 07:28:46 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    return ''.join(char for char in my_string if char not in ('C', 'c'))
+    length = len(my_string)
 
+    j = 0
+
+    new_string = my_string[:]
+
+    for i in range(length):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            j += 1
+
+    return (new_string)
