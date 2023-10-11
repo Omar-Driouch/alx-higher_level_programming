b2b50476b7d8492f0371a8451f0243f8e91efb2c
Omar-Driouch <omardriouch29@gmail.com>
2023-10-11 16:43:44 +0100

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    decimal = 0
+    prev_value = 0
+
+    for numeral in reversed(roman_string):
+        value = roman_dict[numeral]
+        if value < prev_value:
+            decimal -= value
+        else:
+            decimal += value
+        prev_value = value
+
+    return decimal
