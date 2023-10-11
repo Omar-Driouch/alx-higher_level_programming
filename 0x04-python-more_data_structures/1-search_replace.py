2b1b4639ff50a17eb9c13762279100eed8dc4f19
Omar-Driouch <omardriouch29@gmail.com>
2023-10-11 12:56:57 +0100

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = list(my_list)
+    if search <= len(my_list):
+        new_list[search - 1] = replace
+    return new_list
