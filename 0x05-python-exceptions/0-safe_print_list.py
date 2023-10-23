4a88017d9b0c1aedfd6c699a5d4d2040c520af79
Omar-Driouch <omardriouch29@gmail.com>
2023-10-23 11:41:20 +0100

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             i += 1
             print("{}".format(my_list[index]), end="")
         except Exception as inst:
-            print(inst)
+            break
     print("")
     return (i)
