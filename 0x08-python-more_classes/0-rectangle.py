575c58c6e5f2e3c3cb35082f9771f6e3953fcf6c
Omar-Driouch <omardriouch29@gmail.com>
2023-10-30 18:37:59 +0100

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    This class defines a rectangle.
+
+    Attributes:
+        num_rectangles (int): Keeps track of the number of rectangle objects created.
+    """
+    pass
