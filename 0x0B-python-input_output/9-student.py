a5b85f602105068b1fae1c18bbf67c4f6751622e
Omar-Driouch <omardriouch29@gmail.com>
2023-11-07 17:04:11 +0100

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -15,41 +15,10 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-		self._first_name = first_name
-		self._last_name = last_name
-		self._age = age
+		self.first_name = first_name
+		self.last_name = last_name
+		self.age = age
 	
-	@property
-	def	first_name(self):
-		"""Getter for first_name."""
-		return(self._first_name)
-	
-	@first_name.setter
-	def first_name(self, new_value: str):
-		"""Setter for first_name."""
-		self._first_name = new_value
-
-	@property
-	def	last_name(self):
-		"""Getter for last_name."""
-		return(self._last_name)
-	
-	@last_name.setter
-	def last_name(self, new_value: str):
-		"""Setter for last_name."""
-		self._last_name = new_value
-	
-	@property
-	def	age(self):
-		"""Getter for age."""
-		return(self._age)
-	
-	@age.setter
-	def age(self, new_value: str):
-		"""Setter for age."""
-		self._age = new_value
-	
-
 
 	def to_json(self):
 		"""Return the dictionary representation of a Student instance."""
