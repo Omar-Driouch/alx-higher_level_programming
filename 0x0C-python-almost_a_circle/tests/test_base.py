528848b198c7bb32c8798d666156d2eeecdaea67
Omar-Driouch <omardriouch29@gmail.com>
2023-11-13 18:46:57 +0100

    update

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -15,8 +15,7 @@ Unittest classes:
 import os
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+
 
 
 class TestBase_instantiation(unittest.TestCase):
