3f0134777b14d2fc3e4b007cd1cfefc945f3493d
Omar-Driouch <omardriouch29@gmail.com>
2023-11-13 19:24:30 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,6 @@
 """Defines a rectangle class."""
 from models.base import Base
 
-
 class Rectangle(Base):
     """Defines a rectangle class."""
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -37,7 +36,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be greater than 0")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -51,7 +50,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be greater than 0")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -65,7 +64,7 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be greater than or equal to 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -79,5 +78,5 @@ class Rectangle(Base):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be greater than or equal to 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
