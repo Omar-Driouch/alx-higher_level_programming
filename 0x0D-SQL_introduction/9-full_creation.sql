509fe7d7a902b685ebf2ef0ef3975b4aeb340699
Omar-Driouch <omardriouch29@gmail.com>
2023-12-12 20:44:23 +0100

    update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS `hbtn_0c_0`.`second_table` (
-    `id` INT,
-    `name` VARCHAR(256),
-    `score` INT
-);
-INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
