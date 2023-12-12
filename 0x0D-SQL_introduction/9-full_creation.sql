878c1ad68362b467187b1739dbdb10d856344d5d
Omar-Driouch <omardriouch29@gmail.com>
2023-12-12 20:41:06 +0100

    update

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `hbtn_0c_0`.`second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+INSERT INTO `hbtn_0c_0`.`second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
